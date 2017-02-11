dcb83604ca7aab274dbc39f1da9370eaf081ed33
Ben Johnson <Ben Johnson>
2017-02-11 14:37:23 +1100

    Assessment 1: Completed 'Environment and Player Setup'.

---
+++ Roll a ball/Assets/_Scenes/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	private Rigidbody rb;
+
+    public float speed;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void Update()
+	{
+		//Called before rendering a frame
+	}
+
+	void FixedUpdate()
+	{
+		//Called just before any physics calculations are done
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+		rb.AddForce(movement * speed);
+	}
+}
